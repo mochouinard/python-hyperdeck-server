11bdb26a418abf5fdde7722a372db02da6538456
Marc Olivier Chouinard <mochouinard@moctel.com>
2020-09-22 22:00:15 -0400

    Allow all local ip interface to listen to port 9993

--- src/prototype_server.py
+++ src/prototype_server.py
@@ -231,7 +231,7 @@ def client(ip, port, message):
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 9993
+    HOST, PORT = "", 9993
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
         ip, port = server.server_address
