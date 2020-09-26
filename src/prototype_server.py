1ab6f76b536342d061e534595abeed4a16149db4
Marc Olivier Chouinard <mochouinard@moctel.com>
2020-09-26 15:22:31 -0400

    Improvement on loading the first images of the video.  Sadly on my computer, it seem to show the first to frame and pause on the second frame when in paused mode.  If you have active playback, it does go through it quickly.

--- src/prototype_server.py
+++ src/prototype_server.py
@@ -14,10 +14,15 @@ import json
 import re
 
 class HyperDeckPlayer():
-    
+    _playing = False 
+    _debug = False
     def __init__(self):
-        self._instance = vlc.Instance(['--video-on-top', '--start-paused'])
+        self._instance = vlc.Instance(['--video-on-top'])#, '--start-paused'])
+        self._medialist = self._instance.media_list_new()
+        self._listplayer = self._instance.media_list_player_new()
+        #self._listplayer.set_media_player(player)
         self._player = self._instance.media_player_new()
+        self._listplayer.set_media_player(self._player)
         self._player.set_fullscreen(True)
     def time_to_timecode(self, time, fps):
         h = 0
@@ -47,13 +52,45 @@ class HyperDeckPlayer():
         #self._player.set_time(30000)
         finish = 1
     def poschanged(self, event):
+        #print("poschanged")
+        if self._playing == False and self._player.has_vout():
+            self._player.pause()
+
         pass#print(time)
     def ePlaying(self, event):
+        if self._debug:
+            print("ePlaying")
+        if self._playing == False and self._player.has_vout():
+            self._player.pause()
         pass#self.is_playing = True
     def eStopped(self, event):
+        print("eStopped")
         pass#self.is_playing = False
     def ePaused(self, event):
+        if self._debug:
+            print("ePaused")
         pass#self.is_playing = False
+    def ePausable(self, event):
+        if self._playing == False:
+            self._player.pause()
+        if self._debug:
+            print("ePausable")
+    def eUncorked(self, event):
+        print("eUncorked")
+    def eOpening(self, event):
+        if self._debug:
+            print("eOpening")
+    def eBuffering(self, event):
+        if self._debug:
+            print("eBuffering") #, event.getBuffering())
+    def eMediaState(self, event):
+        print ("eMedia", self.media.get_state())
+    def eVout(self, event):
+        if self._debug:
+            print ("eVout")
+        if self._playing == False and self._player.has_vout():
+            self._player.pause()
+
     def is_playing(self):
         return self._player.is_playing()
     def get_fps(self):
@@ -64,7 +101,23 @@ class HyperDeckPlayer():
         self._player.set_rate(rate)
     def load(self, path):
         self.media = self._instance.media_new(path)
+        events = self.media.event_manager()
+
+        events.event_attach(vlc.EventType.MediaStateChanged, self.eMediaState)
+
         self._player.set_media(self.media)
+        #mfps = int(1000 / (self._player.get_fps() or 30))
+
+        #self._player.set_time(1)
+        #t = self._player.get_time()
+        #print("TIME: ", t)
+        #while self._player.get_state() in [vlc.State.Playing, vlc.State.Opening, vlc.State.NothingSpecial]:
+        #    self._player.pause()
+        #    print(self._player.get_state(), self._player.is_playing())
+        #print(self._player.get_state())
+        #self._player.set_time(0)
+        #self._player.pause()
+        print(self._player.get_state())
         #time.sleep(1)
         events = self._player.event_manager()
         events.event_attach(vlc.EventType.MediaPlayerEndReached, self.SongFinished)
@@ -73,6 +126,13 @@ class HyperDeckPlayer():
         events.event_attach(vlc.EventType.MediaPlayerStopped, self.eStopped)
         events.event_attach(vlc.EventType.MediaPlayerPaused, self.ePaused)
         events.event_attach(vlc.EventType.MediaPlayerPositionChanged, self.poschanged)
+        events.event_attach(vlc.EventType.MediaPlayerPausableChanged, self.ePausable)
+        events.event_attach(vlc.EventType.MediaPlayerUncorked, self.eUncorked)
+        events.event_attach(vlc.EventType.MediaPlayerVout, self.eVout)
+        events.event_attach(vlc.EventType.MediaPlayerOpening, self.eOpening)
+        events.event_attach(vlc.EventType.MediaPlayerBuffering, self.eBuffering)
+
+        self._player.play()
 
         self._player.play()
         print ("NEXT")
@@ -81,14 +141,21 @@ class HyperDeckPlayer():
         #self._player.set_time(30000)
         #time.sleep(1)
         #self._player.pause()
-
+        #while self._player.get_state() == 4 or self._player.get_state() == 0:
+        #    print(self._player.get_state())
+        print(self._player.get_state())
+        print("XX", self._player.get_state())
+        
     def play(self):
+        self._playing = True
         self._player.play()
 
     def stop(self):
+        self._playing = False
         self._player.stop()
     def pause(self):
         if self._player.is_playing():
+            self._playing = False
             self._player.pause()
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
