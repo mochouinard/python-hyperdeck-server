ee1eedc3f5dd9a41459d6dd56b9609e6087aa91f
Marc Olivier Chouinard <mochouinard@moctel.com>
2020-09-26 20:18:57 -0400

    Cleaning up by moving a class to it own file

---
+++ src/hdinterface.py
@@ -0,0 +1,103 @@
+from hdplayer import HyperDeckPlayer
+
+import os
+import shlex
+import subprocess
+import json
+
+class HyperDeckInterface:
+    hd =  HyperDeckPlayer()
+    _files = None
+    _active_clip = None
+
+    def ActiveClip(self):
+        if self._active_clip:
+            return str(self._active_clip)
+        else:
+            return 'none'
+
+    def buildSlotInfo(self, slot_id):
+        if slot_id:
+            out = [
+                "slot id: " + str(slot_id),
+                "status: mounted",
+                "volume name: test",
+                "recording time: 0",
+                "video format: 1080p30"
+                ]
+            return out
+        return None
+
+    def get_media(self, clip_id):
+        return self._files[clip_id-1]
+
+    def list_media(self):
+        if self._files == None:
+            self._files = os.listdir('videos')
+
+        return self._files
+    
+    def load_clip(self, clip_id):
+        self.hd.load("videos/" + self.get_media(clip_id))
+        self._active_clip = clip_id
+
+    def buildRemote(self):
+        out = [
+                "enabled: true",
+                "override: false"
+                ]
+        return out
+
+    def buildTransportInfo(self):
+        if self.hd.is_playing():
+            state = 'play'
+        else:
+            state = 'stopped'
+        rate = int(self.hd.get_rate())
+        if rate == 0:
+            rate = 1;
+        speed = str(int(self.hd.get_rate() * 100))
+        (h,m,s,f) = self.hd.time_to_timecode(self.hd.get_time(), self.hd.get_fps())
+        tc = f'{h:02}:{m:02}:{s:02}:{f:02}'
+        out = [
+                'status: ' + state,
+                "speed: " + speed,
+                "slot id: 1",
+                "display timecode: " + tc,
+                "timecode: " + tc,
+                "clip id: " + self.ActiveClip(),
+                "video format: 1080p30",
+                "loop: false"
+                ]
+        return out
+    def ffprobeFind(self, data, field):
+        for item in data['streams']:
+            if field in item:
+                return item[field]
+
+    def findClipMetadata(self, clip_id):
+            return self.findVideoMetada("videos/" + self.get_media(clip_id))
+
+    def findVideoMetada(self, pathToInputVideo):
+        cmd = "ffprobe -v quiet -print_format json -show_streams"
+        args = shlex.split(cmd)
+        args.append(pathToInputVideo)
+        # run the ffprobe process, decode stdout into utf-8 & convert to JSON
+        ffprobeOutput = subprocess.check_output(args).decode('utf-8')
+        ffprobeOutput = json.loads(ffprobeOutput)
+
+        # prints all the metadata available:
+        #import pprint
+        #pp = pprint.PrettyPrinter(indent=2)
+        #pp.pprint(ffprobeOutput)
+
+        # for example, find height and width
+        print(ffprobeOutput)
+        height = ffprobeOutput['streams'][0]['height']
+        width = ffprobeOutput['streams'][0]['width']
+        duration = self.ffprobeFind(ffprobeOutput, 'duration_ts')
+        fps = ffprobeOutput['streams'][0]['avg_frame_rate']
+        #print(width, height, duration, fps)
+        return width, height, duration, fps
+
+
--- src/prototype_server_v2.py
+++ src/prototype_server_v2.py
@@ -11,110 +11,9 @@ import aiohttp
 import async_timeout
 from aiohttp import web
 
-from hdplayer import HyperDeckPlayer
-
-import os
-import shlex
-import subprocess
-import json
+from hdinterface import HyperDeckInterface
 
 import re
-
-class HyperDeckInterface:
-    hd =  HyperDeckPlayer()
-    _files = None
-    _active_clip = None
-
-    def ActiveClip(self):
-        if self._active_clip:
-            return str(self._active_clip)
-        else:
-            return 'none'
-
-    def buildSlotInfo(self, slot_id):
-        if slot_id:
-            out = [
-                "slot id: " + str(slot_id),
-                "status: mounted",
-                "volume name: test",
-                "recording time: 0",
-                "video format: 1080p30"
-                ]
-            return out
-        return None
-
-    def get_media(self, clip_id):
-        return self._files[clip_id-1]
-
-    def list_media(self):
-        if self._files == None:
-            self._files = os.listdir('videos')
-
-        return self._files
-    
-    def load_clip(self, clip_id):
-        self.hd.load("videos/" + self.get_media(clip_id))
-        self._active_clip = clip_id
-
-    def buildRemote(self):
-        out = [
-                "enabled: true",
-                "override: false"
-                ]
-        return out
-
-    def buildTransportInfo(self):
-        if self.hd.is_playing():
-            state = 'play'
-        else:
-            state = 'stopped'
-        rate = int(self.hd.get_rate())
-        if rate == 0:
-            rate = 1;
-        speed = str(int(self.hd.get_rate() * 100))
-        (h,m,s,f) = self.hd.time_to_timecode(self.hd.get_time(), self.hd.get_fps())
-        tc = f'{h:02}:{m:02}:{s:02}:{f:02}'
-        out = [
-                'status: ' + state,
-                "speed: " + speed,
-                "slot id: 1",
-                "display timecode: " + tc,
-                "timecode: " + tc,
-                "clip id: " + self.ActiveClip(),
-                "video format: 1080p30",
-                "loop: false"
-                ]
-        return out
-    def ffprobeFind(self, data, field):
-        for item in data['streams']:
-            if field in item:
-                return item[field]
-
-    def findClipMetadata(self, clip_id):
-            return self.findVideoMetada("videos/" + self.get_media(clip_id))
-
-    def findVideoMetada(self, pathToInputVideo):
-        cmd = "ffprobe -v quiet -print_format json -show_streams"
-        args = shlex.split(cmd)
-        args.append(pathToInputVideo)
-        # run the ffprobe process, decode stdout into utf-8 & convert to JSON
-        ffprobeOutput = subprocess.check_output(args).decode('utf-8')
-        ffprobeOutput = json.loads(ffprobeOutput)
-
-        # prints all the metadata available:
-        #import pprint
-        #pp = pprint.PrettyPrinter(indent=2)
-        #pp.pprint(ffprobeOutput)
-
-        # for example, find height and width
-        print(ffprobeOutput)
-        height = ffprobeOutput['streams'][0]['height']
-        width = ffprobeOutput['streams'][0]['width']
-        duration = self.ffprobeFind(ffprobeOutput, 'duration_ts')
-        fps = ffprobeOutput['streams'][0]['avg_frame_rate']
-        #print(width, height, duration, fps)
-        return width, height, duration, fps
-
 
 async def ws_handler(websocket, path):
     while True:
@@ -311,17 +210,11 @@ class HDServer:
 async def serve():                                                                                           
     # Telnet
     hds = HDServer()
-    ts = await asyncio.start_server( hds.new_conn, '127.0.0.1', 9993)
+    ts = await asyncio.start_server( hds.new_conn, '', 9993)
   
     addr = ts.sockets[0].getsockname()
-    print(f'Serving on {addr}')
+    print(f'Serving HyperDeck Server on {addr}')
 
-    
-    #async with ts:
-    #    await ts.serve_forever()
-        #async with server:
-    #await ts.wait_closed()
-    #    serve_forever()
     
     #http
     app = aiohttp.web.Application()
@@ -330,11 +223,11 @@ async def serve():
     app.router.add_static('/videos/', path='videos/', name='static')
     runner = aiohttp.web.AppRunner(app)
     await runner.setup()
-    site = aiohttp.web.TCPSite(runner, 'localhost', '8082')
+    site = aiohttp.web.TCPSite(runner, '', '8082')
     await site.start()
 
     #ws
-    wsserver = await websockets.serve(ws_handler, 'localhost', 8765)
+    wsserver = await websockets.serve(ws_handler, '', 8765)
     await wsserver.wait_closed()
 
 
